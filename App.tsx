93c6580df00e8c8f3b3b069fd8ed77f38a67dbdf
BrunoMacedo1986 <bruno.macedo9@fatec.sp.gov.br>
2025-02-22 00:52:27 +0000

    Exemplo Click Alert React

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import MeuBotao from './Componentes/MeuBotao';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.titulo}>Parangaricutirrimiruaru</Text>
+      <MeuBotao titulo="APERTE-ME" 
+      onPress={()=>{Alert.alert("ATENÇÃO", "BOTÃO CLICADO")}}>
+        
+      </MeuBotao> 
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +21,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+    margin: 10,
+    borderRadius: 30,
+    elevation: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 5,
+      height: 10,
+    }
   },
   titulo:
   {
